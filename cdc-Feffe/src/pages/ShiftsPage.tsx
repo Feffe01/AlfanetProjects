7c03f5905faf055b32a972b85557091d64c23aa3
Feffe01 <fede.tempemail@gmail.com>
2024-11-03 21:19:00 +0100

    Added activity table in shifts page, but still not working

--- cdc-Feffe/src/pages/ShiftsPage.tsx
+++ cdc-Feffe/src/pages/ShiftsPage.tsx
@@ -1,6 +1,6 @@
 import { Box, CircularProgress, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import { DataGrid, GridRowsProp, GridColDef, GridColumnMenuProps, GridColumnMenu, GridCellParams } from '@mui/x-data-grid';
+import { DataGrid, GridRowsProp, GridColDef, GridColumnMenuProps, GridColumnMenu, GridCellParams, GridRowId } from '@mui/x-data-grid';
 import { useEffect, useState } from 'react';
 import { shiftsEndpoint, shiftTypesEndpoint } from '../constants/endpoints'; // Assuming you have an updateShiftEndpoint
 import { GET, PUT } from '../constants/httpRequests';
@@ -8,33 +8,46 @@ import WeekPicker from '../components/WeekPicker';
 import dayjs, { Dayjs } from 'dayjs';
 
 interface ShiftType {
-  ID: number;
-  Nome: string;
+	ID: number;
+	Nome: string;
 	color: string;
 }
 
 interface Shift {
-  ID: number;
-  Nome: string;
-  Cognome: string;
-  turni: Array<{
-    id: number;
-    Data: string;
-    NomeGiorno: string;
-    Id_turno: number;
-    nome_turno: string;
-  }>;
+	ID: number;
+	Nome: string;
+	Cognome: string;
+	turni: Array<{
+		id: number;
+		Data: string;
+		NomeGiorno: string;
+		Id_turno: number;
+		nome_turno: string;
+	}>;
+}
+
+interface Activity {
+	IDTimbratura: number,
+	ingresso: string,
+	uscita: string,
+}
+
+interface ActivitiesTableRow {
+	id: number,
+	entrance: string,
+	exit: string,
+	hourCount: string,
 }
 
 function CustomColumnMenu(props: GridColumnMenuProps) {
-  return (
-    <GridColumnMenu
-      {...props}
-      slots={{
-        columnMenuColumnsItem: null,
-      }}
-    />
-  );
+	return (
+		<GridColumnMenu
+			{...props}
+			slots={{
+				columnMenuColumnsItem: null,
+			}}
+		/>
+	);
 }
 
 export default function ShiftsPage() {
@@ -51,9 +64,9 @@ export default function ShiftsPage() {
 		{
 			field: 'Lunedì', headerName: 'LUNEDÌ',
 			renderHeader: () => (
-				<span style={{textAlign:"center"}}>
-						{shifts[0]?.turni[0]?.NomeGiorno}<br />
-						{shifts[0]?.turni[0]?.Data}
+				<span style={{ textAlign: "center" }}>
+					{shifts[0]?.turni[0]?.NomeGiorno}<br />
+					{shifts[0]?.turni[0]?.Data}
 				</span>
 			), minWidth: 100, maxWidth: 150, flex: 1,
 			editable: true, type: 'singleSelect', valueOptions: shiftTypes.map(type => type.Nome),
@@ -62,9 +75,9 @@ export default function ShiftsPage() {
 		{
 			field: 'Martedì', headerName: 'MARTEDÌ',
 			renderHeader: () => (
-				<span style={{textAlign:"center"}}>
-						{shifts[0]?.turni[1]?.NomeGiorno}<br />
-						{shifts[0]?.turni[1]?.Data}
+				<span style={{ textAlign: "center" }}>
+					{shifts[0]?.turni[1]?.NomeGiorno}<br />
+					{shifts[0]?.turni[1]?.Data}
 				</span>
 			), minWidth: 100, maxWidth: 150, flex: 1,
 			editable: true, type: 'singleSelect', valueOptions: shiftTypes.map(type => type.Nome),
@@ -73,9 +86,9 @@ export default function ShiftsPage() {
 		{
 			field: 'Mercoledì', headerName: 'MERCOLEDÌ',
 			renderHeader: () => (
-				<span style={{textAlign:"center"}}>
-						{shifts[0]?.turni[2]?.NomeGiorno}<br />
-						{shifts[0]?.turni[2]?.Data}
+				<span style={{ textAlign: "center" }}>
+					{shifts[0]?.turni[2]?.NomeGiorno}<br />
+					{shifts[0]?.turni[2]?.Data}
 				</span>
 			), minWidth: 100, maxWidth: 150, flex: 1,
 			editable: true, type: 'singleSelect', valueOptions: shiftTypes.map(type => type.Nome),
@@ -84,9 +97,9 @@ export default function ShiftsPage() {
 		{
 			field: 'Giovedì', headerName: 'GIOVEDÌ',
 			renderHeader: () => (
-				<span style={{textAlign:"center"}}>
-						{shifts[0]?.turni[3]?.NomeGiorno}<br />
-						{shifts[0]?.turni[3]?.Data}
+				<span style={{ textAlign: "center" }}>
+					{shifts[0]?.turni[3]?.NomeGiorno}<br />
+					{shifts[0]?.turni[3]?.Data}
 				</span>
 			), minWidth: 100, maxWidth: 150, flex: 1,
 			editable: true, type: 'singleSelect', valueOptions: shiftTypes.map(type => type.Nome),
@@ -95,9 +108,9 @@ export default function ShiftsPage() {
 		{
 			field: 'Venerdì', headerName: 'VENERDÌ',
 			renderHeader: () => (
-				<span style={{textAlign:"center"}}>
-						{shifts[0]?.turni[4]?.NomeGiorno}<br />
-						{shifts[0]?.turni[4]?.Data}
+				<span style={{ textAlign: "center" }}>
+					{shifts[0]?.turni[4]?.NomeGiorno}<br />
+					{shifts[0]?.turni[4]?.Data}
 				</span>
 			), minWidth: 100, maxWidth: 150, flex: 1,
 			editable: true, type: 'singleSelect', valueOptions: shiftTypes.map(type => type.Nome),
@@ -106,9 +119,9 @@ export default function ShiftsPage() {
 		{
 			field: 'Sabato', headerName: 'SABATO',
 			renderHeader: () => (
-				<span style={{textAlign:"center"}}>
-						{shifts[0]?.turni[5]?.NomeGiorno}<br />
-						{shifts[0]?.turni[5]?.Data}
+				<span style={{ textAlign: "center" }}>
+					{shifts[0]?.turni[5]?.NomeGiorno}<br />
+					{shifts[0]?.turni[5]?.Data}
 				</span>
 			), minWidth: 100, maxWidth: 150, flex: 1,
 			editable: true, type: 'singleSelect', valueOptions: shiftTypes.map(type => type.Nome),
@@ -117,9 +130,9 @@ export default function ShiftsPage() {
 		{
 			field: 'Domenica', headerName: 'DOMENICA',
 			renderHeader: () => (
-				<span style={{textAlign:"center"}}>
-						{shifts[0]?.turni[6]?.NomeGiorno}<br />
-						{shifts[0]?.turni[6]?.Data}
+				<span style={{ textAlign: "center" }}>
+					{shifts[0]?.turni[6]?.NomeGiorno}<br />
+					{shifts[0]?.turni[6]?.Data}
 				</span>
 			), minWidth: 100, maxWidth: 150, flex: 1,
 			editable: true, type: 'singleSelect', valueOptions: shiftTypes.map(type => type.Nome),
@@ -219,14 +232,94 @@ export default function ShiftsPage() {
 		return { ...newRow };
 	};
 
-  // PAGE
-  const theme = useTheme();
+	// SECOND TABLE
+	// HANDLING ROW SELECTION
 
-  return (
-    <Box
-      width={"100%"} height={"100%"} padding={"40px 5%"}
-      display={"flex"} flexDirection={"column"} alignItems={"center"} gap={"20px"}
-      sx={{
+	const [activities, setActivities] = useState<Array<Activity>>([]);
+
+	const activitiesColumns: GridColDef[] = [
+		{
+			field: 'name', headerName: '', minWidth: 100, maxWidth: 150, flex: 2,
+			headerAlign: "center", headerClassName: "header",
+		},
+		{
+			field: 'hours', headerName: 'Ore', minWidth: 100, maxWidth: 150, flex: 2,
+			headerAlign: "center", headerClassName: "header",
+		},
+		{
+			field: 'target', headerName: 'Target', minWidth: 100, maxWidth: 150, flex: 2,
+			headerAlign: "center", headerClassName: "header",
+		},
+		{
+			field: 'space', headerName: '', minWidth: 100, maxWidth: 150, flex: 2,
+			headerAlign: "center", headerClassName: "header",
+		},
+		{
+			field: 'insertedProd', headerName: 'Produzione Inserita', minWidth: 100, maxWidth: 150, flex: 2,
+			headerAlign: "center", headerClassName: "header",
+		},
+		{
+			field: 'verifiedProd', headerName: 'Produzione Verificata', minWidth: 100, maxWidth: 150, flex: 2,
+			headerAlign: "center", headerClassName: "header",
+		},
+	];
+
+	// STAMPINGS ROWS ADAPTATION
+	const transformDataForActivitiesTable = () => {
+		if (!activities.length) return [];
+
+		const transformedRows = activities.map((activity) => {
+			const ingresso = dayjs(activity.ingresso, "HH:mm:ss");
+			const uscita = dayjs(activity.uscita, "HH:mm:ss");
+
+			const diffInMinutes = uscita.diff(ingresso, "seconds");
+			const hours = Math.floor(diffInMinutes / 60 / 60);
+			const minutes = Math.floor(diffInMinutes / 60);
+			const seconds = diffInMinutes % 60;
+
+			const hourCount = `${hours} ore ${minutes} min ${seconds} sec`;
+
+			const row: ActivitiesTableRow = {
+				id: activity.IDTimbratura,
+				entrance: activity.ingresso,
+				exit: activity.uscita,
+				hourCount: hourCount,
+			};
+
+			return row;
+		});
+
+		console.log(transformedRows);
+		return transformedRows;
+	};
+
+	const activitiesRows: GridRowsProp = transformDataForActivitiesTable();
+
+	const [showActivitiesTable, setShowActivitiesTable] = useState<"block" | "none">("none");
+	const [shiftsTableHeight, setShiftsTableHeight] = useState<"auto" | "200px">("auto");
+
+	const handleRowSelection = (rowId: GridRowId) => {
+		const selectedActivity: Array<Activity> = shifts.find(shift => shift.ID === rowId)?.Timbrature || [];
+
+		if (selectedActivity[0]) {
+			setActivities(selectedActivity);
+			setShiftsTableHeight("200px");
+			setShowActivitiesTable("block");
+		}
+		else {
+			setShiftsTableHeight("auto");
+			setShowActivitiesTable("none");
+		}
+	}
+
+	// PAGE
+	const theme = useTheme();
+
+	return (
+		<Box
+			width={"100%"} height={"100%"} padding={"40px 5%"}
+			display={"flex"} flexDirection={"column"} alignItems={"center"} gap={"20px"}
+			sx={{
 				'& .header': {
 					backgroundColor: theme.palette.secondary.main,
 					color: 'black',
@@ -251,17 +344,17 @@ export default function ShiftsPage() {
 					color: 'black',
 				},
 			}}
-    >
-      <Typography variant={"h2"} color={"primary"} fontWeight={800} textAlign={"center"} gutterBottom>
-        TABELLA TURNI
-      </Typography>
-      <WeekPicker onWeekChange={(startOfWeek: Dayjs) => setWeekStart(startOfWeek)} />
-      <Box height={"500px"} width={"100%"}>
+		>
+			<Typography variant={"h2"} color={"primary"} fontWeight={800} textAlign={"center"} gutterBottom>
+				TABELLA TURNI
+			</Typography>
+			<WeekPicker onWeekChange={(startOfWeek: Dayjs) => setWeekStart(startOfWeek)} />
+			<Box height={shiftsTableHeight} maxHeight={"500px"} width={"100%"}>
 				{isLoading ?
 					<Box display="flex" justifyContent="center" alignItems="center" height={"100%"}>
 						<CircularProgress />
 					</Box>
-				:
+					:
 					<DataGrid rows={rows} columns={columns}
 						slots={{ columnMenu: CustomColumnMenu }}
 						showCellVerticalBorder
@@ -276,7 +369,14 @@ export default function ShiftsPage() {
 						disableMultipleRowSelection
 					/>
 				}
-      </Box>
-    </Box>
-  );
+			</Box>
+			<Box height={"auto"} maxHeight={"500px"} width={"100%"} display={showActivitiesTable}>
+				<DataGrid rows={activitiesRows} columns={activitiesColumns}
+					showCellVerticalBorder
+					disableRowSelectionOnClick
+					disableMultipleRowSelection
+				/>
+			</Box>
+		</Box>
+	);
 }
